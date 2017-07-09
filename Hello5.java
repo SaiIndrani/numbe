3ca80891a0fd9ec09137c83ef5b9b58ecc80aa94
SaiIndrani <saiindrani_chintala@srmuniv.edu.in>
2017-07-09 14:58:59 +0530

    Add files via upload

---
+++ Hello5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class HELLO
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x;
+	  for(x=0;x<5;x++)
+	  System.out.println("HELLO");
+	 
+	}
+}
