1cee6ff06f2aa6b3f823c8649af3930e8f9f05e6
SaiIndrani <saiindrani_chintala@srmuniv.edu.in>
2017-07-09 15:04:18 +0530

    Add files via upload

---
+++ naturalno.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class natural number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x,i;
+	  int y=0;
+	 Scanner s= new Scanner(System.in);
+	 x=s.nextInt();
+	 for(i=1;i<x;i++)
+	 y=y+i;
+	 System.out.println("sum:"+y);
+  	}
+}
